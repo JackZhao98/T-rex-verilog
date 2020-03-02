c6d25434226318bd38fd52c33e8306e48719a7e0
Jack Zhao <jack_zhao98@outlook.com>
2020-03-01 21:29:35 -0800

    2020-03-01  Jack Zhao  <jackzhao@Jacks-MacBook-Pro.local>
    	* DinoFSM.v: FSM module for T-Rex movement.
    
    	* Gravity.v: v0.1 Initialze Gravity module. Initial speed and gravity are passed as parameters. Both variables are expected to be integers, and InitialSpeed is expected to be divided by 2 if squared (i.e. IS^2/2 is a whole number).
    
    	* draw*.v: All drawing modules are updated.

--- DinoFSM.v
+++ DinoFSM.v
@@ -1,23 +1,42 @@
 module DinoFSM(
-	       input wire   clk,
-	       input wire   rst,
-	       input wire   animationClk,
-	       input wire   Airborne,
-	       input wire   onGround,
-	       input wire   isDead,
-	       input wire   isPaused,
- 	       output [3:0] DinoMovementSelect);
+	       input wire 	 clk,
+	       input wire 	 rst,
+	       input wire 	 animationClk,
+	       input wire 	 Airborne,
+	       input wire 	 onGround,
+	       input wire 	 isDuck,
+	       input wire 	 isDead,
+	       input wire 	 isPaused,
+ 	       output wire [3:0] DinoMovementSelect);
 
-   reg [3:0] 		    select;
+   reg [3:0] 			 select;
    
    always @(posedge clk or posedge rst) begin
       if (rst) begin
 	 select <= 4'b0000;
       end
 
-      if (isDead) begin
-	 select <= 4'b0000;
-	 
-      if (onGround) begin
-	 
-	       
+      else if (isDead) begin
+	 select <= 4'b0010;
+      end
+
+      else if (Airborne) begin
+	 select <= 4'b0011;
+      end
+      
+      else if (isPaused) begin
+	 select <= 4'b0011;
+      end
+
+      else if (onGround) begin
+	 if (animationClk)
+	   select <= (isDuck)? 4'b0101:4'b0001;
+	 else
+	   select <= (isDuck)? 4'b0000:4'b0100;
+      end
+
+   end // always @ (posedge clk or posedge rst)
+   
+   assign DinoMovementSelect = select;
+   
+endmodule // DinoFSM
