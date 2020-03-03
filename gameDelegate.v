3d6e80586f38e83e673c875e3dc060619deafe5d
Jack Zhao <jack_zhao98@outlook.com>
2020-03-02 23:22:52 -0800

    Game delegate added

--- gameDelegate.v
+++ gameDelegate.v
@@ -1,20 +1,26 @@
 module GameDelegate(
 		    input wire clk,
 		    input wire rst,
+        input wire collided,
 		    output wire [1:0] gameState);
    
   reg [1:0] state;
 
   localparam InitState = 2'b00;
   localparam InGameState = 2'b01;
-  localparam PrepareState = 2'b10;
-  localparam DeadState = 2'b11;
+  //localparam PrepareState = 2'b10;
+  localparam DeadState = 2'b10;
   
   always @(posedge clk) begin
   	if (rst) begin
-  		state <= InitState;
+  	  state <= InitState;
   	end
 
+    else if (collided) begin
+      state <= DeadState;
+    end
+
+    
 
   end
 
